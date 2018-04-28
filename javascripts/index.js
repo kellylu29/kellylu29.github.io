f4e5529c058df448a05a4af95b32e5c9bc044558
Kelly <kellylovelu@gmail.com>
2018-04-28 19:56:52 -0400

    add listeners and handlers to pop up content

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,11 +1,19 @@
 window.onload = () => {
+	let projects = document.querySelector('.projects');		
+	let about = document.querySelector('.about');		
+	let contact = document.querySelector('.contact');
+	let closeBtn = document.querySelector('.close button');	
+
 	let scrollToFixLinks = () => {
 		let container = document.querySelector('.one');
-		let links = document.querySelector('.links-container')
+		let links = document.querySelector('.links-container');
 	
 		window.onscroll = (event) => {
 			if (this.pageYOffset > 445) {
 				links.classList.add('fix-position');
+				projects.classList.remove('visible');
+				about.classList.remove('visible');
+				contact.classList.remove('visible');
 			} else {
 				links.classList.remove('fix-position');
 			}
@@ -17,7 +25,33 @@ window.onload = () => {
 
 		linksContainer.addEventListener('click', (event) => {
 			event.preventDefault();
+
+			if (event.target.textContent === 'Projects') {
+				projects.classList.add('visible');
+				closeBtn.classList.add('visible');
+				window.scrollTo(0,0);
+				hidePopUpPage(projects);				
+			}
+			if (event.target.textContent === 'About') {
+				about.classList.add('visible');
+				closeBtn.classList.add('visible');
+				window.scrollTo(0, 0);
+				hidePopUpPage(about);				
+			}
+			if (event.target.textContent === 'Contact') {
+				contact.classList.add('visible');
+				closeBtn.classList.add('visible');
+				window.scrollTo(0, 0);
+				hidePopUpPage(contact);
+			}
 		});
+	}
+
+	let hidePopUpPage = (element) => {
+		closeBtn.addEventListener('click', () => {
+			element.classList.remove('visible');
+			closeBtn.classList.remove('visible');
+		})
 	}
 
 	scrollToFixLinks();
